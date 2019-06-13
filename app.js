53aa5cdc9814ec823ddc2950edea68e66afc604d
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-06-12 20:18:14 -0400

    added routes for save button

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ client.connect()
 const express = require("express");
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 //instance of express 
 const app = express();
+
+//read the request data from the frontend
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 //host the files in public folder to localhost
@@ -38,8 +42,7 @@ app.set('view engine', 'handlebars');
 
 
 /*
-render the index file into the main default layout at the / root dir
-set html title to notepad */
+render the index file into the main default layout at the / root dir */
 app.get('/', (req, res) => {
 	client.query('SELECT paragraph FROM document_tbl', (err, res2) => {
 	//data inside column 1 (paragraph), row 0	
@@ -47,16 +50,30 @@ app.get('/', (req, res) => {
 		res.render('index', {
 			title: 'Notepad',
 			content: data,
-			style: 'styles.css'
+			style: 'styles.css',
+			script: 'appPost.js'
 		});	
 	});
 });
 
 /*
-////////////////////////////////////////////////////////////////
-need to handle put request from client in backend
-////////////////////////////////////////////////////////////////
-*/
+Save button handling post request from client 
+Responds to the put request on / root dir
+Update the paragraph string in the db with the textarea text and render the table with the data from the table */
+app.put('/', urlencodedParser, (req, res) => {
+	console.log(req.body.text);
+	const textAreaData = req.body.text; //textArea text
+	const data = textAreaData.replace(/^\s+/g, ''); //regex textArea with leading text removed
+	const id = 0;
+	client.query('UPDATE document_tbl SET paragraph = $1 WHERE id = $2',[data, id], (err, res2) => {
+		res.render('index', {
+			title: 'Notepad',
+			content: data,
+			style: 'styles.css',
+			script: 'appPost.js'
+		});	
+	});
+});
 
 
 //Start the localhost on port 3000
